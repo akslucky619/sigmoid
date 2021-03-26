c9ff077ddd871624359685a0a92b5a4c4e1afd26
akash@619 <Akash.sharma1@in.ey.com>
2021-03-27 01:59:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Layout from "./Layout";
-
+let s = true;
 function App() {
-  return (
-    <div className="App">
-      <Layout />
-    </div>
-  );
+  return <div className="App">{s && <Layout />}</div>;
 }
 
 export default App;
