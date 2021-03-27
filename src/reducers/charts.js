d2436c1018071a2c65e8705954faf95ce6e2228f
akash@619 <Akash.sharma1@in.ey.com>
2021-03-28 00:14:34 +0530

    update

--- src/reducers/charts.js
+++ src/reducers/charts.js
@@ -1,38 +1,52 @@
-import { getPieData, getTableData, getBarData } from "../api/charts";
+import { getChartData } from "../api/charts";
 import { pieBody, pieUrl } from "../helpers/apiHelpers/pie";
 import { barBody, barUrl } from "../helpers/apiHelpers/bar";
 import { tableBody, tableUrl } from "../helpers/apiHelpers/table";
 
-const SET_USER = "SET_USER";
+const SET_PIE_CHART_DATA = "SET_PIE_CHART_DATA";
+const SET_TABLE_CHART_DATA = "SET_TABLE_CHART_DATA";
+const SET_BAR_CHART_DATA = "SET_BAR_CHART_DATA";
 
-const setUser = (token) => ({
-  type: SET_USER,
-  token,
+const setPieChartData = (pieData) => ({
+  type: SET_PIE_CHART_DATA,
+  pieData,
+});
+const setTableChartData = (tableData) => ({
+  type: SET_TABLE_CHART_DATA,
+  tableData,
+});
+const setBarChartData = (barData) => ({
+  type: SET_BAR_CHART_DATA,
+  barData,
 });
 
 const init = (userToken) => async (dispatch, getstate) => {
-  //   console.log(userBody, loginUrl);
-  console.log({
-    pieBody,
-    barBody,
-    tableBody,
-    pieUrl,
-    barUrl,
-    tableUrl,
-    userToken,
-  });
-  const pieData = await getPieData(pieUrl, pieBody, userToken);
-  const tableData = await getTableData(tableUrl, tableBody, userToken);
-  const barData = await getBarData(barUrl, barBody, userToken);
-  console.log({ pieData });
-  console.log({ tableData });
-  console.log({ barData });
+  try {
+    const piePromise = getChartData(pieUrl, pieBody, userToken);
+    const tablePromise = getChartData(tableUrl, tableBody, userToken);
+    const barPromise = getChartData(barUrl, barBody, userToken);
+    const [pieApiData, tableApiData, barApiData] = await Promise.all([
+      piePromise,
+      tablePromise,
+      barPromise,
+    ]);
 
-  //   dispatch(setUser(token));
+    console.log({ pieApiData });
+    console.log({ tableApiData });
+    console.log({ barApiData });
+    dispatch(setPieChartData(pieApiData));
+    dispatch(setTableChartData(tableApiData));
+    dispatch(setBarChartData(barApiData));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const defaultState = {
   userToken: null,
+  pieData: [],
+  tableData: [],
+  barData: [],
 };
 
 export const ACTIONS = {
@@ -41,9 +55,17 @@ export const ACTIONS = {
 
 function charts(state = defaultState, action) {
   switch (action.type) {
-    case SET_USER:
+    case SET_PIE_CHART_DATA:
       return Object.assign({}, state, {
-        userToken: action.token,
+        pieData: action.pieData,
+      });
+    case SET_TABLE_CHART_DATA:
+      return Object.assign({}, state, {
+        tableData: action.tableData,
+      });
+    case SET_BAR_CHART_DATA:
+      return Object.assign({}, state, {
+        barData: action.barData,
       });
     default:
       return state;
